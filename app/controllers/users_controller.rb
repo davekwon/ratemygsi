c0c3ab807954e0a574dfdbf61fae183c47c7aea5
Dave Kwon <benfeb23@gmail.com>
2017-11-14 18:25:49 -0800

    minor changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,11 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   end
+
+  def destroy
+    User.find(user[:user_id]).destroy      
+    user[:user_id] = nil         
+    redirect_to 'root'
+  end  
+
 end
